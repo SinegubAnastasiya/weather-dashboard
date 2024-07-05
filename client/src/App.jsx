bfb12c70d8e3f96b41eda201c53b978df78e90e4
SinegubAnastasiya <fedukevich98@mail.ru>
2024-07-05 17:44:21 +0300

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import style from './app.module.css'
 
 function App() {
+  const key = '8d165bffa133ae36abcfbc06e6b92c93'
+  const [weather, setWeather] = useState('')
+  const [city, setCity] = useState('')
+
+  const getWeather = async () => {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    setWeather(response.data);
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
 
   return (
     <>
-      <div>
-        
+      <div className={style.app}>
+        <div className={style.container}>
+          <div className={style.top}>
+            <div className={style.location}>
+              <p>Minsk</p>
+            </div>
+            <div className={style.temp}>
+              <p>60°F</p>
+            </div>
+            <div className={style.description}>
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className={style.bottom}></div>
+        </div>
       </div>
     </>
   )
